64684e286bdae314b6704126956bd52c31d6fdea
arshadp024 <pirjadearshad358@gmail.com>
2026-08-12 23:54:58 +0530

    .

--- C++/Array/Easy/question2.cpp
+++ C++/Array/Easy/question2.cpp
@@ -1,22 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int SecondlargestElement(vector<int>& nums) {
-   int largestElement=nums[0];
-   int SecondlargestElement=nums[0];
-      for(int i=0;i<=nums.size()-2;i++){
+  int largestElement=nums[0];
+      for(int i=0;i<nums.size()-1;i++){
         if(largestElement<nums[i+1]){
             largestElement=nums[i+1];
         }
       }
-      for(int i=0;i<=nums.size()-2;i++){
-        if((SecondlargestElement<nums[i+1])&&nums[i+1]!=largestElement){
-            SecondlargestElement=nums[i+1];
-        }
-      }
-      if(largestElement>SecondlargestElement){
-      return SecondlargestElement;
-    }else{
-        return -1;
-    }
+      for(int i=0;i<=nums.size()-1;i++){
+      if(largestElement=nums[i])
 }
+}
+
     

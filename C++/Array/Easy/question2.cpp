080d7f1a00099e12ed740ad398623ad1062a6476
arshadp024 <pirjadearshad358@gmail.com>
2026-08-13 00:24:06 +0530

    .

--- C++/Array/Easy/question2.cpp
+++ C++/Array/Easy/question2.cpp
@@ -1,15 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int SecondlargestElement(vector<int>& nums) {
-  int largestElement=nums[0];
-      for(int i=0;i<nums.size()-1;i++){
-        if(largestElement<nums[i+1]){
-            largestElement=nums[i+1];
+int getSecondLargest(vector<int> &arr)
+{
+    int largestElement = arr[0];
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        if (largestElement < arr[i + 1])
+        {
+            largestElement = arr[i + 1];
         }
-      }
-      for(int i=0;i<=nums.size()-1;i++){
-      if(largestElement=nums[i])
-}
-}
+    }
+    for (int i = 0; i != arr.size();)
+    {
+        if (largestElement == arr[i])        //!In these condition if i is erased i+1 index becomes i so segmentation error occurs to avoid it use these type of for loop
+        {
+            arr.erase(arr.begin() + i);
+        }
+        else
+        {
+            i++;
+        }
+        if (arr.size() == 0)
+        {                                   //!If each of element is same then 
+            return -1;
+        }
+    }
+    int SecondlargestElement = arr[0];
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        if (SecondlargestElement < arr[i + 1])
+        {
+            SecondlargestElement = arr[i + 1];
+        }
+    }
+    if (largestElement > SecondlargestElement)
+    {
+        return SecondlargestElement;
+    }
+    else
+    {
+        return -1;
+    }
+    // class Solution {
+    //   public:
+    //     int getSecondLargest(vector<int> &arr) {
+    //       int largestElement=arr[0];
+    //       for(int i=0;i<arr.size()-1;i++){
+    //         if(largestElement<arr[i+1]){
+    //             largestElement=arr[i+1];
+    //         }
+    //       }
+    //       for(int i=0;i!=arr.size();){
+    //       if(largestElement==arr[i]){
+    //         arr.erase(arr.begin()+i);
+    //       }else{
+    //           i++;
+    //       }
+    //       if(arr.size()==0){
+    //           return -1;
+    //       }
+    //       }
+    //       int SecondlargestElement=arr[0];
+    //       for(int i=0;i<arr.size()-1;i++){
+    //         if(SecondlargestElement<arr[i+1]){
+    //             SecondlargestElement=arr[i+1];
+    //         }
 
-    
+    // }
+    // if(largestElement>SecondlargestElement){
+    //     return SecondlargestElement;
+    // }else{
+    //     return -1;
+    // }
+    //     }
+    //     };

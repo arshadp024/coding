67dbcda39807ee2461b244dbd4cef7a4b361b1e2
arshadp024 <pirjadearshad358@gmail.com>
2026-08-12 19:20:25 +0530

    .

--- C++/Array/Easy/question1.cpp
+++ C++/Array/Easy/question1.cpp
@@ -3,9 +3,7 @@ using namespace std;
 int largestElement(vector<int>& nums) {
     int largestElement=nums[0];
       for(int i=0;i<nums.size()-2;i++){
-        if(nums[i]>nums[i+1]){
-            largestElement=nums[i];
-        }else{
+        if(largestElement<nums[i+1]){
             largestElement=nums[i+1];
         }
       }
@@ -15,3 +13,16 @@ int largestElement(vector<int>& nums) {
         vector<int> v={-4,-3,-1,-11,-8};
         cout<<largestElement(v);
     }
+//     class Solution {
+//   public:
+//     int largest(vector<int> &arr) {
+//         int largestElement=arr[0];
+//       for(int i=0;i<arr.size()-1;i++){
+//         if(largestElement<arr[i+1]){
+//             largestElement=arr[i+1];
+//         }
+//       }
+//       return largestElement;
+//     }
+    
+// };

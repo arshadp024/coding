1240a32f1c05145c595c5655c35893f7ec24fd58
arshadp024 <pirjadearshad358@gmail.com>
2026-08-13 16:11:17 +0530

    .

---
+++ C++/Array/Easy/question4.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> removeDuplicates(vector<int> &arr)
+{
+    vector<int> newarr;
+    int temp = arr[0];
+    newarr.push_back(temp);
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        if ((arr[i] = arr[i + 1]) && temp != arr[i + 1])
+        {
+            newarr.push_back(arr[i]);
+            temp = arr[i];
+        }
+        else if (arr[i] < arr[i + 1])
+        {
+            newarr.push_back(arr[i]);
+        }
+    }
+    return newarr;
+}
+int main(){
+    vector<int> v={2,3,3,3,5};
+    
+    for(auto i:removeDuplicates(v)){
+        cout<<i<<endl;;
+    }
+    return 0;
+}
+// class Solution {
+//   public:
+//     vector<int> removeDuplicates(vector<int> &arr) {
+//         vector<int> newarr;
+//         int temp=arr[0];
+//         newarr.push_back(temp);
+//         for(int i=0;i<arr.size()-1;i++){
+//             if((arr[i]==arr[i+1])&&temp!=arr[i+1]){
+//                 newarr.push_back(arr[i]);
+//                 temp=arr[i];
+//             }else if(arr[i]<arr[i+1]){
+//                 newarr.push_back(arr[i]);
+//             }
+//         }
+//         return newarr;
+//     }
+// };

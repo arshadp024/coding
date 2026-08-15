4de198cb10677789f582b5704bc8d6a4cfc68481
arshadp024 <pirjadearshad358@gmail.com>
2026-08-15 18:23:16 +0530

    .

--- C++/Array/Easy/question4.cpp
+++ C++/Array/Easy/question4.cpp
@@ -1,32 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-vector<int> removeDuplicates(vector<int> &arr)
-{
-    vector<int> newarr;
-    int temp = arr[0];
-    newarr.push_back(temp);
-    for (int i = 0; i < arr.size() - 1; i++)
-    {
-        if ((arr[i] = arr[i + 1]) && temp != arr[i + 1])
-        {
-            newarr.push_back(arr[i]);
-            temp = arr[i];
+  vector<int> removeDuplicates(vector<int> &arr) {
+        vector<int> newarr;
+        int temp=arr[0];
+        newarr.push_back(temp);
+        for(int i=0;i<arr.size()-1;i++){
+            if((arr[i]=arr[i+1])&&temp!=arr[i+1]){
+                newarr.push_back(arr[i]);
+                temp=arr[i];
+            }else if(arr[i]<arr[i+1]){
+                newarr.push_back(arr[i]);
+            }
         }
-        else if (arr[i] < arr[i + 1])
-        {
-            newarr.push_back(arr[i]);
+        return newarr;
+    }
+    int main(){
+        vector<int>v ={1,23,23,44};
+        for(auto i:removeDuplicates(v)){
+            cout<<i<<endl;
         }
+        return 0;
     }
-    return newarr;
-}
-int main(){
-    vector<int> v={2,3,3,3,5};
-    
-    for(auto i:removeDuplicates(v)){
-        cout<<i<<endl;;
-    }
-    return 0;
-}
 // class Solution {
 //   public:
 //     vector<int> removeDuplicates(vector<int> &arr) {
@@ -34,7 +28,7 @@ int main(){
 //         int temp=arr[0];
 //         newarr.push_back(temp);
 //         for(int i=0;i<arr.size()-1;i++){
-//             if((arr[i]==arr[i+1])&&temp!=arr[i+1]){
+//             if((arr[i]=arr[i+1])&&temp!=arr[i+1]){
 //                 newarr.push_back(arr[i]);
 //                 temp=arr[i];
 //             }else if(arr[i]<arr[i+1]){

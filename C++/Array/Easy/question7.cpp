01c469fff4708e9a29f3e2c0f260d1a8840e8d13
arshadp024 <pirjadearshad358@gmail.com>
2026-08-16 13:33:54 +0530

    .

--- C++/Array/Easy/question7.cpp
+++ C++/Array/Easy/question7.cpp
@@ -1,3 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+void moveZeroes(vector<int> &arr)
+{
+    int j = -1;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == 0)
+        {
+            j = i;
+            break;
+        }
+    }
+    for (int i = j + 1; i < arr.size(); i++)
+    {
+        if (arr[i] != 0)
+        {
+            swap(arr[j], arr[i]);
+            j++;
+        }
+    }
+}
+int main(){
+    vector<int> v={0,1,4,0,5,2};
+    moveZeroes(v);
+    for(auto i:v){
+        cout<<i<<endl;
+    }
+}
+// class Solution {
+//   public:
+//     void pushZerosToEnd(vector<int>& arr) {
+//         int j = -1;
+//             for (int i = 0; i < arr.size(); i++)
+//             {
+//                 if (arr[i] == 0)
+//                 {
+//                     j = i;
+//                     break;
+//                 }
+//             }
+//             if(j==-1){
+//                 return;
+//             }
+//             for (int i = j + 1; i < arr.size(); i++)
+//             {
+//                 if (arr[i] != 0)
+//                 {
+//                     swap(arr[j], arr[i]);
+//                     j++;
+//                 }
+//             }
+//     }
+// };
+
 // class Solution {
 // public:
 //     void moveZeroes(vector<int>& nums) {

523d4c0b2cab7cb6c3727eaaadef4637ed7eda07
arshadp024 <pirjadearshad358@gmail.com>
2026-08-28 16:55:20 +0530

    .

--- C++/Array/Medium/question2.cpp
+++ C++/Array/Medium/question2.cpp
@@ -35,22 +35,26 @@ public:
     }
 };
 ----------------------dutch national flag algorithm------------------------------
-#include <bits/stdc++.h>
-void sortArray(vector<int>& arr, int n)
+class Solution
 {
-    int low = 0, mid = 0, high = n - 1;
+public:
+    void sortColors(vector<int> &nums)
+    {
+  int n=nums.size();
+  int low = 0, mid = 0, high = n - 1;
     while(mid <= high) {
-        if(arr[mid] == 0) {
-            swap(arr[low], arr[mid]);
+        if(nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
             low++;
             mid++;
         }
-        else if(arr[mid] == 1) {
+        else if(nums[mid] == 1) {
             mid++;
         }
         else {
-            swap(arr[mid], arr[high]);
+            swap(nums[mid], nums[high]);
             high--;
         }
     }
-}
+    }
+};

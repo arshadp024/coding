7882a6412a5ab156dee6dfca78f321b790bb372b
arshadp024 <pirjadearshad358@gmail.com>
2026-08-28 16:06:26 +0530

    .

--- C++/Array/Medium/question2.cpp
+++ C++/Array/Medium/question2.cpp
@@ -1,25 +1,33 @@
-class Solution {
+class Solution
+{
 public:
-    void sortColors(vector<int>& nums) {
-        int temp=0;
-        int n=nums.size();
-        for(int i=temp;i<n;i++){
-           if(nums[i]==0){
-            swap(nums[temp],nums[i]);
-            temp++;
-           }
+    void sortColors(vector<int> &nums)
+    {
+        int temp = 0;
+        int n = nums.size();
+        for (int i = temp; i < n; i++)
+        {
+            if (nums[i] == 0)
+            {
+                swap(nums[temp], nums[i]);
+                temp++;
+            }
         }
-           for(int i=temp;i<n;i++){
-           if(nums[i]==1){
-            swap(nums[temp],nums[i]);
-            temp++;
-           }
-           }
-           for(int i=temp;i<n;i++){
-           if(nums[i]==2){
-            swap(nums[temp],nums[i]);
-            temp++;
-           }
+        for (int i = temp; i < n; i++)
+        {
+            if (nums[i] == 1)
+            {
+                swap(nums[temp], nums[i]);
+                temp++;
+            }
+        }
+        for (int i = temp; i < n; i++)
+        {
+            if (nums[i] == 2)
+            {
+                swap(nums[temp], nums[i]);
+                temp++;
+            }
         }
     }
 };

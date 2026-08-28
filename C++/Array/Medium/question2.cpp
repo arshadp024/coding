e723d7d3907a8fd236655e0008d633658f22c700
arshadp024 <pirjadearshad358@gmail.com>
2026-08-28 16:49:40 +0530

    .

--- C++/Array/Medium/question2.cpp
+++ C++/Array/Medium/question2.cpp
@@ -1,3 +1,6 @@
+------------------------ Brute -----------------------------------------------------
+                     merge Sort 
+----------------------------------------optimal------------------------------------------                     
 class Solution
 {
 public:
@@ -31,3 +34,23 @@ public:
         }
     }
 };
+----------------------dutch national flag algorithm------------------------------
+#include <bits/stdc++.h>
+void sortArray(vector<int>& arr, int n)
+{
+    int low = 0, mid = 0, high = n - 1;
+    while(mid <= high) {
+        if(arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}

edc665806ddf4f61c53d947fd96bd1f73ff34dfc
arshadp024 <pirjadearshad358@gmail.com>
2026-09-01 01:22:39 +0530

    .

--- C++/Array/Medium/question11.cpp
+++ C++/Array/Medium/question11.cpp
@@ -24,3 +24,81 @@ public:
         }
     }
 };
+---------------------------------------------------Better--------------------------------------------------------------
+class Solution {
+public:
+    vector<vector<int>> setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int col[m] = {0};
+        int row[n] = {0};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        return matrix;
+    }
+};
+-------------------------------------------Optimal---------------------------------------------------------------------------
+class Solution {
+public:
+    vector<vector<int>> setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        // int col[m] = {0}; -> matrix[0][..]
+        // int row[n] = {0}; -> matrix[..][0]
+        int col0 = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark the i-th row
+                    matrix[i][0] = 0;
+
+                    // mark the j-th col
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != 0) {
+                    // check for col & row
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) matrix[0][j] = 0;
+        }
+
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) matrix[i][0] = 0;
+        }
+
+        return matrix;
+    }
+};

509507407e753ef2af22d42e9326f984a57f84c7
arshadp024 <pirjadearshad358@gmail.com>
2026-09-01 17:48:17 +0530

    .

---
+++ C++/Array/Medium/question12.cpp
@@ -0,0 +1,39 @@
+-------------------------------------------Better--------------------------------------------
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        vector<vector<int>> v(matrix.size(), vector<int>(matrix.size()));
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix.size();j++){
+                v[i][j]=matrix[i][j];
+            }
+        }
+        for(int j=0;j<v.size();j++){
+            for(int i=0;i<v.size();i++){
+                 matrix[i][j]=v[v.size()-1-j][i];
+            }
+        }
+        
+    }
+};
+--------------------------------------optimal-----------------------------------------------
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+
+                int temp = matrix[i][j];
+
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] =
+                    matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] =
+                    matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+};

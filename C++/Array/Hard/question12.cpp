c3c78c0accb1a3a6e8b68bbf2186d604fe8face2
arshadp024 <pirjadearshad358@gmail.com>
2026-09-12 12:01:37 +0530

    .

--- C++/Array/Hard/question12.cpp
+++ C++/Array/Hard/question12.cpp
@@ -17,3 +17,31 @@ public:
     }
 };
 ---------------------------------------------------Optimal----------------------------------------------------------
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int mul1=nums[0];
+        int largest_product1=mul1;
+        int mul2=nums[n-1];
+        int largest_product2=mul2;
+        for(int i=1;i<n;i++){
+            if(mul1==0){
+                mul1=nums[i];
+            }else{
+                mul1*=nums[i];
+            }
+            largest_product1=max(largest_product1,mul1);
+        }
+        for(int i=n-2;i>-1;i--){
+            if(mul2==0){
+                mul2=nums[i];
+            }else{
+                mul2*=nums[i];
+            }
+            largest_product2=max(largest_product2,mul2);
+        }
+        int largest_product=max(largest_product1,largest_product2);
+        return largest_product;
+    }
+};

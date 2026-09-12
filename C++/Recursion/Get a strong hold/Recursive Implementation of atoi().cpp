28503231f518db7ac4aa1b2e859d76aaa4f758aa
arshadp024 <pirjadearshad358@gmail.com>
2026-09-13 01:58:58 +0530

    .

--- C++/Recursion/Get a strong hold/Recursive Implementation of atoi().cpp
+++ C++/Recursion/Get a strong hold/Recursive Implementation of atoi().cpp
@@ -44,3 +44,47 @@ public:
     }
 };
 -----------------------------------------------optimal--------------------------------------------------------------
+//* s.erase in recursive function takes O(n) time complexity , so i changed that....
+class Solution {
+public:
+    int myAtoi(string s) {
+        int temp = INT_MIN;
+        int b=rem_space(s, 0);
+        if (s.length() > 0 && s[b] == '+') {
+            temp = 1;
+            s.erase(s.begin()+b);
+        } else if (s.length() > 0 && s[b] == '-') {
+            temp = 0;
+            s.erase(s.begin()+b);
+        }
+        long long a = printnum(s, b, 0);
+        if (temp == 0) {
+            a = -a;
+        }
+        if (a > 2147483647) {
+            return 2147483647;
+        }
+        if (a < -2147483648) {
+            return -2147483648;
+        }
+        return a;
+    }
+    long long printnum(string& s, int i, long long num) {
+        if (num > 2147483647) {
+            return 2147483648;
+        }
+        if (i >= s.length() || (s[i] < '0' || s[i] > '9'))
+            return num;
+        num = 10 * num + (s[i] - '0');
+        return printnum(s, i + 1, num);
+    }
+    int rem_space(string& s, int i) {
+        if (i >= s.length()) {
+            return i;
+        }
+        if (s[i] != ' ') {
+            return i;
+        }
+        return rem_space(s, i+1);
+    }
+};

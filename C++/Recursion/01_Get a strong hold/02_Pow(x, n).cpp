baf54b439be5de309a8f16b6e2596e9e5c7dda50
arshadp024 <pirjadearshad358@gmail.com>
2026-09-13 03:05:04 +0530

    .

--- C++/Recursion/01_Get a strong hold/02_Pow(x, n).cpp
+++ C++/Recursion/01_Get a strong hold/02_Pow(x, n).cpp
@@ -27,4 +27,28 @@ public:
        return power(x,a-1,m);
     }
 };
---------------------------------------------------------Optimal--------------------------------------------------
+--------------------------------------------------------Optimal(Recursive)(Striver)--------------------------------------------------
+class Solution {
+public:
+    double helper(double x, long long n) {
+        if (n == 0) return 1.0;
+        
+        double half = helper(x, n / 2);
+    
+        if (n % 2 == 0) {
+            return half * half;
+        } 
+        else {
+            return half * half * x;
+        }
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1.0 / x;
+            N = -N;
+        }
+        return helper(x, N);
+    }
+};

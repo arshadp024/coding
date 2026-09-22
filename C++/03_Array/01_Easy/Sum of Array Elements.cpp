559ded2569603119606568c228296d07cd2930e9
arshadp024 <pirjadearshad358@gmail.com>
2026-09-23 00:16:03 +0530

    .

---
+++ C++/03_Array/01_Easy/Sum of Array Elements.cpp
@@ -0,0 +1,11 @@
+---------------------------------------------New Striver(Optimal)---------------------------
+class Solution {
+   public:
+    int sum(int arr[], int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+};

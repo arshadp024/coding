707f17ecddc5af4d1f1adb3d0df162c76a29afb4
arshadp024 <pirjadearshad358@gmail.com>
2026-09-24 18:34:47 +0530

    .

---
+++ C++/03_Array/01_Easy/Remove Duplicate from sorted array.cpp
@@ -0,0 +1,26 @@
+------------------------------------New Striver(STL Optimal)---------------------------------------
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        nums.erase(unique(nums.begin(),nums.end()),nums.end());
+        return nums.size();
+    }
+};
+-----------------------------------(Optimal)---------------------------------
+class Solution {
+   public:
+    int removeDuplicates(vector<int>& nums) {
+        int temp = nums[0];
+        int left = 1;
+        int right = 1;
+        while (right < nums.size()) {
+            if (temp != nums[right]) {
+                temp = nums[right];
+                swap(nums[left], nums[right]);
+                left++;
+            }
+            right++;
+        }
+        return left;
+    }
+};

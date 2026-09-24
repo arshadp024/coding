c50d977005352c817616a18b03750bb93dba7912
arshadp024 <pirjadearshad358@gmail.com>
2026-09-24 17:12:02 +0530

    .

--- C++/01_Learn the basics/05_String/Largest Odd Number in String.cpp
+++ C++/01_Learn the basics/05_String/Largest Odd Number in String.cpp
@@ -10,3 +10,22 @@ public:
         return "";
     }
 };
+----------------------------------------New Striver(Optimal)--------------------
+class Solution{	
+public:		
+    string largeOddNum(string& s){
+        int temp=-1;
+        for(int i=0;i<s.size();i++){
+            if((s[i]-'0')!=0){
+                temp=i;
+                break;
+            }
+        }
+        for(int i=s.size()-1;i>=temp;i--){
+            if((s[i]-'0')%2!=0){
+                return s.substr(temp,i+1-temp);   //! The substr uses the starting point and the size it we want the sub string
+            }
+        }
+        return "";
+    }
+};
